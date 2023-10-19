2fe0f1f6e322c7193bc5314b42defce0cf8501fc
Victor Ayala <victor.ayala2a@gmail.com>
2023-10-19 02:45:51 -0300

    Config test

--- othello.py
+++ othello.py
@@ -25,8 +25,8 @@ class Othello:
     def play_test_min_max(self):
         # Si es alto el cutting y el tiempo es el mismo se nota cuantos nodos se visitan
         # Sin alpha beta usan siempre su max_cutting_time
-        min_max_black = MinMax(10, 50, False)
-        min_max_white = MinMax(10, 50, True)
+        min_max_black = MinMax(8, 50, False)
+        min_max_white = MinMax(8, 50, True)
         current_player = BLACK
         while not is_game_over(self.board):
             player = "Black" if current_player == BLACK else "White"
